8f51ad666d53c070d78ee36129d192cdabe6fbaf
XorTroll <xabofdez@outlook.es>
2018-12-22 18:05:06 +0100

    Add a new example, document both

--- Examples/LoopUpdating/Include/MainApplication.hpp
+++ Examples/LoopUpdating/Include/MainApplication.hpp
@@ -1,5 +1,4 @@
 
-
 #pragma once
 
 // Include Plutonium
@@ -10,9 +9,10 @@ class Layout1 : public pu::Layout
 {
     public:
         Layout1();
+        void Update();
     private:
         // An easy way to keep objects is to have them as private pointer members
-        pu::element::TextBlock *helloText;
+        pu::element::TextBlock *sampleText;
 };
 
 // Define your application as a class too
--- Examples/SimpleApplication/Source/MainApplication.cpp
+++ Examples/SimpleApplication/Source/MainApplication.cpp
@@ -1,13 +1,19 @@
 #include <MainApplication.hpp>
 
+// Implement all the layout/application functions here
+
 Layout1::Layout1()
 {
+    // Create the textblock with the text we want
     this->helloText = new pu::element::TextBlock(300, 300, "Hello world!");
+    // Add the textblock to the layout's element container. IMPORTANT! this MUST be done, having them as members is not enough (just a simple way to keep them)
     this->AddChild(this->helloText);
 }
 
 MainApplication::MainApplication()
 {
+    // Create the layout (calling the constructor above)
     this->layout1 = new Layout1();
+    // Load the layout. In applications layouts are loaded, not added into a container (you don't select a added layout, just load it from this function)
     this->LoadLayout(this->layout1);
 }
