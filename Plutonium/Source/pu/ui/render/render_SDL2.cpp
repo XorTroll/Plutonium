e4c8505c959370276304c70fdab47e632947ea89
XorTroll <xabofdez@outlook.es>
2019-07-01 13:26:37 +0200

    Fix wrapping

--- Plutonium/Source/pu/ui/render/render_SDL2.cpp
+++ Plutonium/Source/pu/ui/render/render_SDL2.cpp
@@ -20,7 +20,7 @@ namespace pu::ui::render
 
     NativeTexture RenderText(NativeFont Font, String Text, Color Color)
     {
-        NativeSurface txsrf = TTF_RenderUNICODE_Blended(Font, (const u16*)Text.CStr(), { Color.R, Color.G, Color.B, Color.A });
+        NativeSurface txsrf = TTF_RenderUNICODE_Blended_Wrapped(Font, (const u16*)Text.CStr(), { Color.R, Color.G, Color.B, Color.A }, 1280);
         SDL_SetSurfaceAlphaMod(txsrf, 255);
         return ConvertToTexture(txsrf);
     }
