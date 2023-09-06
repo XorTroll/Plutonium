b56564b70d038c59eef875f2c3cf436859c827f2
XorTroll <xabofdez@gmail.com>
2023-09-06 20:12:58 +0200

    Add removed header docs

--- Plutonium/include/pu/audio/audio_Sfx.hpp
+++ Plutonium/include/pu/audio/audio_Sfx.hpp
@@ -1,4 +1,16 @@
 
+/*
+
+    Plutonium library
+
+    @file audio_Sfx.hpp
+    @brief Sfx (sound effects) support
+    @author XorTroll
+
+    @copyright Plutonium project - an easy-to-use UI framework for Nintendo Switch homebrew
+
+*/
+
 #pragma once
 #include <SDL2/SDL_mixer.h>
 #include <pu/pu_Include.hpp> 
